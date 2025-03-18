4d8c7615b73531267d67a22b1582209625d02c66
OWNER <dbtjdwnbs@gmail.com>
2025-03-18 14:44:34 +0900

    Initial commit

---
+++ app/src/main/java/com/example/test_text/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.test_text;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText eText;
+    private Button eButton;
+    private TextView eTextView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        eText =(EditText) findViewById(R.id.edittext);
+        eButton = (Button) findViewById(R.id.button);
+        eTextView =(TextView) findViewById(R.id.textView);
+    }
+
+    public void onClicked(View view) {
+        String str = eText.getText().toString();
+        eTextView.setText(str);
+        String str = eText.getText().toString();
+        eTextView.setText(str);
+        String str = eText.getText().toString();
+        eTextView.setText(str);
+
+    }
+}
